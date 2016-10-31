2dfad4f2554295a27bae0be68ac6720c6ffe5724
Jose Gutierrez <gutiory@gmail.com>
2016-10-31 17:35:16 +0100

    Exercises Chapter 4

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -58,9 +58,33 @@ object Option {
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m,2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =  sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =  (a, b) match {
+    case (None, _) => None
+    case (_, None) => None
+    case (Some(x), Some(y)) => Some(f(x,y)).orElse(None)
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def map22[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+      a.flatMap(a1 => b.map(b1 => f(a1, b1)))
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h.flatMap(h1 => sequence(t).map(h1 :: _))
+  }
+
+  def sequenceFold[A](a: List[Option[A]]): Option[List[A]] = {
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x,y)(_ :: _))
+  }
+
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+  }
+
+  def traverseFold[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil))((x, y) => map2(f(x),y)(_ :: _))
+
+  def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
+    traverseFold(a)(x => x)
 }
