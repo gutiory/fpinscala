b88d2d061974e456dfc92404a5de3249bdbb6994
Jose Gutierrez <gutiory@gmail.com>
2016-11-06 23:48:11 +0100

    Exercise 5.13

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -11,7 +11,7 @@ trait Stream[+A] {
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -61,6 +61,30 @@ trait Stream[+A] {
   def flatMap[B](f: A => Stream[B]) : Stream[B] =
     foldRight(Stream[B]())((h, t) => f(h).append(t))
 
+  // mapUnfold, takeUnfold, takeWhileUnfold, zipWith throw "is not a member of Stream[A]"
+  def mapUnfold[B](f: A => B) : Stream[B] = unfold(this){
+    case Cons(h, t) => Some((f(h()),t()))
+    case _ => None
+  }
+
+  def takeUnfold(n: Int) : Stream [A] = unfold(this, n){
+    case (Cons(h, t), cont) => if(cont == 0) None else Some((h() , (t(), cont - 1)))
+    case _ => None
+  }
+
+  def takeWhileUnfold(p: A => Boolean) : Stream[A] = unfold(this){
+    case Cons(h, t) if(p(h())) => Some((h(), t()))
+    case _ => None
+  }
+
+  def zipWith[B, C](b : Stream[B], f: (A,B) => C) : Stream[C] = unfold(this, b) {
+      case (Cons(ha, ta), Cons(hb, tb)) => Some(f(ha(), hb()), (ta(), tb()))
+      case _ => None
+  }
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] = ???
+
+
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
 }
 case object Empty extends Stream[Nothing]
@@ -76,19 +100,31 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
 
-  def constant[A](a: A) : Stream[A] = Stream.cons(a, constant(a))
+  def constant[A](a: A): Stream[A] = Stream.cons(a, constant(a))
 
   def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
 
   def fibs(): Stream[Int] = {
-    def loop(n_1: Int, n_2: Int) : Stream[Int] = cons(n_2, loop(n_1 + n_2, n_1))
+    def loop(n_1: Int, n_2: Int): Stream[Int] = cons(n_2, loop(n_1 + n_2, n_1))
     loop(1, 0)
   }
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    if (f(z) == None) empty
+    else cons(f(z).get._1, unfold(f(z).get._2)(f))
+
+
+  def onesUnfold(): Stream[Int] = unfold(1)((x => Some(1, 1)))
+
+  def constantUnfold[A](a: A): Stream[A] = unfold(a)((x => Some(x, x)))
+
+  def fromUnfold(n: Int): Stream[Int] = unfold(n)(x => Some(x, x + 1))
+
+  def fibsUnfold() = unfold((0, 1)) { case (n_1, n_2) => Some((n_1, (n_2, n_1 + n_2))) }
+
 }
